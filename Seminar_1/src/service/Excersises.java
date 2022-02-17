e6d0bdf31b4c77d73c41e260e0f794eeeb6ea294
Jekabs Stikans <s8_stikan_j@venta.lv>
2022-02-17 10:09:48 +0200

    Hello world example

--- Seminar_1/src/service/Excersises.java
+++ Seminar_1/src/service/Excersises.java
@@ -1,12 +1,20 @@
 package service;
 
+/**
+ * 1. seminâra uzdevumi JAVA kursâ
+ * @author Jçkabs Stikâns
+ * 
+ */
+
 public class Excersises 
 {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+		
+		System.out.print("Hello world!\n");
+		System.out.print("Hello world!\n");
 	}
 
 }
